d716a67c0b0e88e46a503d7a58d9327271c90b77
CreatureSurvive <dbuehre@me.com>
2021-05-29 14:41:14 -0500

    Updated to v1.0.2
    Added library validation and media library entitlements
    Mimic Preferences app delegate/application to prevent crashing in some tweaks
    Fixed a permissions issue preventing settings from loading/saving correctly on Taurine

--- TweakSettings/TSAppDelegate.h
+++ TweakSettings/TSAppDelegate.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface TSAppDelegate : UIResponder <UIApplicationDelegate>
+@interface TSAppDelegate : UIApplication <UIApplicationDelegate>
 
 @property(strong, nonatomic) UIWindow *window;
 
--- TweakSettings/main.m
+++ TweakSettings/main.m
@@ -29,6 +29,6 @@ int main(int argc, char *argv[]) {
 
     }
 
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+    return UIApplicationMain(argc, argv, appDelegateClassName, appDelegateClassName);
 
 }
